3597e4a3802da8abb01d38ca262d90be85d9b15a
Apprentice <apprentice@dbc24.local>
2013-07-02 10:54:10 -0700

    new project. created models/migrations for articles, categories, and tags.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SweetArticlesRails::Application.config.secret_token = '383e72067bce5ee6c7038ce36d7bbe8b8aa062cfee4ece1485739751d80e8a5addd1a877b1177aecb7b458abeaca47bb6a91aa5cc402a6e7fb0e8336d64b7204'
